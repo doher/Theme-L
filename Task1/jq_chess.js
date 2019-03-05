ed6c9ffd8c1baa876eabe83bf0e6cbcee841c7df
doher <alexandr.pletezhov@gmail.com>
2019-03-05 13:19:33 +0300

    added unoptimized solution

--- Task1/jq_chess.js
+++ Task1/jq_chess.js
@@ -5,15 +5,25 @@
             'background-color': 'rgb(129, 194, 127)'
         })
 
-        $('table.CHESS').find('tr').each(function (i) {
-            $(this).children('td').each(function (j) {
-                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
-                    $(this).addClass('white');
-                } else {
-                    $(this).addClass('black');
-                }
-            })
-        })
+        $('table.CHESS').find('tr:odd').each(function () {
+            $(this).children('td:odd').each(function () {
+                $(this).addClass('white');
+            });
+
+            $(this).children('td:even').each(function () {
+                $(this).addClass('black');
+            });
+        });
+
+        $('table.CHESS').find('tr:even').each(function () {
+            $(this).children('td:odd').each(function () {
+                $(this).addClass('black');
+            });
+
+            $(this).children('td:even').each(function () {
+                $(this).addClass('white');
+            });
+        });
 
         $('.white').css({
             'background-color': 'white'
@@ -21,6 +31,30 @@
 
         $('.black').css({
             'background-color': 'black'
+        });
+
+        $('table.CHESS').find('tr:first').each(function () {
+            $(this).children('td').css({
+                'background-color': 'rgb(129, 194, 127)'
+            })
+        });
+
+        $('table.CHESS').find('tr:last').each(function () {
+            $(this).children('td').css({
+                'background-color': 'rgb(129, 194, 127)'
+            })
+        });
+
+        $('table.CHESS').find('tr').each(function () {
+            $(this).children('td:first').css({
+                'background-color': 'rgb(129, 194, 127)'
+            })
+        });
+
+        $('table.CHESS').find('tr').each(function () {
+            $(this).children('td:last').css({
+                'background-color': 'rgb(129, 194, 127)'
+            })
         });
     });
 })($);
